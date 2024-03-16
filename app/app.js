d56b26a2e836c90fb08d508114aaaae0a92d4d3f
Soumen Bhattacharjee <bhattacharjee.soumen@gmail.com>
2024-03-16 11:31:40 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -15,9 +15,9 @@ const MONGO_DB_HOST = process.env.MONGO_DB_HOST;
 
 //changes for docker file
 
-let mongoUrlDockerCompose = `mongodb://${DB_USER}:${DB_PASSWORD}@${MONGO_DB_HOST}/reminders`
+//let mongoUrlDockerCompose = `mongodb://${DB_USER}:${DB_PASSWORD}@${MONGO_DB_HOST}/reminders`
 
-//let mongoUrlDockerCompose = `mongodb://admin:password@mongodb:27017/reminders`
+let mongoUrlDockerCompose = `mongodb://admin:password@mongodb:27017/reminders`
 
 mongoose.connect(mongoUrlDockerCompose, {
     authSource: 'admin'
